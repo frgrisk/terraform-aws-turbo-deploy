3090290ed5935ebf17091e5542fcd51ed80da3d7
lbazran <luqman.bazran@frgrisk.com>
2026-01-29 15:04:50 +0800

    add policies to allow lambda to publish to sns topic

--- sns.tf
+++ sns.tf
@@ -8,3 +8,22 @@ resource "aws_sns_topic_subscription" "email_subscription" {
   protocol  = "email"
   endpoint  = var.admin_email
 }
+
+# Define the policy as a data source
+data "aws_iam_policy_document" "sns_publish_policy" {
+  statement {
+    effect    = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_role.terraform_lambda_role]
+    }
+    actions   = ["SNS:Publish"]
+    resources = [aws_sns_topic.terraform_failures.arn]
+  }
+}
+
+# Attach the policy to the SNS topic
+resource "aws_sns_topic_policy" "allow_publish" {
+  arn    = aws_sns_topic.terraform_failures.arn
+  policy = data.aws_iam_policy_document.sns_publish_policy.json
+}
